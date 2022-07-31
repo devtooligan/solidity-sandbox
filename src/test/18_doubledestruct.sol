febd51a0274b33ced5f1ea316e6670b515638fe0
Richie <devtooligan@gmail.com>
2022-07-31 10:00:02 -0700

    doubledestruct

---
+++ src/test/18_doubledestruct.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+import "forge-std/Test.sol";
+
+contract Destructor {
+    constructor() {
+        selfdestruct(payable(address(0xBADBABE)));
+    }
+}
+
+contract Test18 is Test {
+    function shortlived() public {
+        bytes memory initCode = abi.encodePacked(type(Destructor).creationCode);
+        uint256 salt = 0xB0FFED;
+        address addr;
+        assembly {
+            let codeSize := mload(initCode) // get size of initCode
+            addr := create2(
+                0, // 0 wei
+                add(initCode, 32), // the bytecode starts at the second slot. The first slot contains array length.
+                codeSize, // size of init_code
+                salt // salt from function arguments
+            )
+        }
+    }
+
+    function test_singledestruct() external {
+        shortlived();
+    }
+
+    function test_doubledestruct() external {
+        shortlived();
+        shortlived();
+    }
+
+}
